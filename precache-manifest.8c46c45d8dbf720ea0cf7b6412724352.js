1b8d7e977c64c54bed342a14e415bf717f3d6901
szkapec <szkapec999@o2.pl>
2020-10-22 21:10:10 +0200

    Updates

---
+++ precache-manifest.8c46c45d8dbf720ea0cf7b6412724352.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d451d268f21d8fc1c84c3a341297d59",
+    "url": "/mateusz-kapron/index.html"
+  },
+  {
+    "revision": "ab22d1e54f5671d4a46d",
+    "url": "/mateusz-kapron/static/css/main.2a7d4cbd.chunk.css"
+  },
+  {
+    "revision": "760fbfc0cfa8773645a1",
+    "url": "/mateusz-kapron/static/js/2.c927ed4f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mateusz-kapron/static/js/2.c927ed4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab22d1e54f5671d4a46d",
+    "url": "/mateusz-kapron/static/js/main.537969f5.chunk.js"
+  },
+  {
+    "revision": "f0c3d7a31f74bd90fb6e",
+    "url": "/mateusz-kapron/static/js/runtime-main.ae3f294a.js"
+  },
+  {
+    "revision": "1acf12649c0fc70b4441430701fc1587",
+    "url": "/mateusz-kapron/static/media/Git_icon.1acf1264.svg"
+  },
+  {
+    "revision": "cffb3a523b38e9b9f42a40d8a2a89fdb",
+    "url": "/mateusz-kapron/static/media/React-node-express2.cffb3a52.PNG"
+  },
+  {
+    "revision": "6f65452a0230f48bd606026c98adaa54",
+    "url": "/mateusz-kapron/static/media/Roboto.6f65452a.png"
+  },
+  {
+    "revision": "bd1876685d668756086ca7c2110558ec",
+    "url": "/mateusz-kapron/static/media/ajax-icon-7.bd187668.jpg"
+  },
+  {
+    "revision": "7cb3310c8f072628f1b07e60918fa261",
+    "url": "/mateusz-kapron/static/media/analytics.7cb3310c.png"
+  },
+  {
+    "revision": "c5ee926c48b4159b25070b7d6f32b1c8",
+    "url": "/mateusz-kapron/static/media/animate.c5ee926c.svg"
+  },
+  {
+    "revision": "9f3d7fa7ff2f6a829420413e67763dd0",
+    "url": "/mateusz-kapron/static/media/astronomy.9f3d7fa7.jpg"
+  },
+  {
+    "revision": "b7cc81630a4ed0c43950641cec13f415",
+    "url": "/mateusz-kapron/static/media/baflix.b7cc8163.png"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/mateusz-kapron/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "7c06579e7d93600d13f9753329a10a80",
+    "url": "/mateusz-kapron/static/media/bowling.7c06579e.PNG"
+  },
+  {
+    "revision": "1583c2ef7c3f911013bdc69594b03a25",
+    "url": "/mateusz-kapron/static/media/course.1583c2ef.PNG"
+  },
+  {
+    "revision": "1ca58dac83a6d7bb9be8217a48f45ad7",
+    "url": "/mateusz-kapron/static/media/cpp.1ca58dac.svg"
+  },
+  {
+    "revision": "2db97d69ba44f8da4663584e8197143f",
+    "url": "/mateusz-kapron/static/media/css-3.2db97d69.svg"
+  },
+  {
+    "revision": "d8d66a1a081323b1ea648c788e4bd26c",
+    "url": "/mateusz-kapron/static/media/ecommers2.d8d66a1a.PNG"
+  },
+  {
+    "revision": "386cc387066317cf209c9abf7e98afab",
+    "url": "/mateusz-kapron/static/media/english.386cc387.svg"
+  },
+  {
+    "revision": "d5f66342991130a4c118dffb1f962717",
+    "url": "/mateusz-kapron/static/media/express.d5f66342.png"
+  },
+  {
+    "revision": "c509a4c46008514b5a5084d2596c99e4",
+    "url": "/mateusz-kapron/static/media/facebook.c509a4c4.svg"
+  },
+  {
+    "revision": "5f9ff958693895381771f360b869f1d2",
+    "url": "/mateusz-kapron/static/media/firebase.5f9ff958.png"
+  },
+  {
+    "revision": "c32de339fbedc02b106b140cf6db8489",
+    "url": "/mateusz-kapron/static/media/generate.c32de339.PNG"
+  },
+  {
+    "revision": "0555bfb7e81f57a1efe6a53c7490abb7",
+    "url": "/mateusz-kapron/static/media/github.0555bfb7.svg"
+  },
+  {
+    "revision": "23a7dee5995afbb2f9ad3b32799c6f82",
+    "url": "/mateusz-kapron/static/media/gmail.23a7dee5.svg"
+  },
+  {
+    "revision": "7334348ad6f656dffe78adc0d45ced53",
+    "url": "/mateusz-kapron/static/media/html-5.7334348a.svg"
+  },
+  {
+    "revision": "13a096531e50277a3fb17460c39db221",
+    "url": "/mateusz-kapron/static/media/java.13a09653.svg"
+  },
+  {
+    "revision": "7fd504665575a4ea5ed926fbfa839f68",
+    "url": "/mateusz-kapron/static/media/javascript-4.7fd50466.svg"
+  },
+  {
+    "revision": "14f7b6753cb58692c0da571c41b33210",
+    "url": "/mateusz-kapron/static/media/linkedin.14f7b675.svg"
+  },
+  {
+    "revision": "e5eee315a17de0d7f56117077eb71fa9",
+    "url": "/mateusz-kapron/static/media/mongo.e5eee315.png"
+  },
+  {
+    "revision": "991dcc53afa2e5b8bd8006c3b0110473",
+    "url": "/mateusz-kapron/static/media/mountains-desktop.991dcc53.jpg"
+  },
+  {
+    "revision": "53655b533b12a438f8988377c55c415b",
+    "url": "/mateusz-kapron/static/media/mountains-mobile-small.53655b53.jpg"
+  },
+  {
+    "revision": "957f2dc9411f8ca53a468ee0760ad832",
+    "url": "/mateusz-kapron/static/media/mountains-mobile.957f2dc9.jpg"
+  },
+  {
+    "revision": "ff18385bbe199c754c52a18a6f6bbc4a",
+    "url": "/mateusz-kapron/static/media/mysql-5.ff18385b.svg"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/mateusz-kapron/static/media/nodejs-icon.b337a2fb.svg"
+  },
+  {
+    "revision": "a81109727be4e9767a1cd6c9be837b45",
+    "url": "/mateusz-kapron/static/media/people.a8110972.svg"
+  },
+  {
+    "revision": "939a1c6112ce9438a03b2ec10aa10a42",
+    "url": "/mateusz-kapron/static/media/phone-call.939a1c61.svg"
+  },
+  {
+    "revision": "84412bcca8b1988652fddb63b6af32bf",
+    "url": "/mateusz-kapron/static/media/poeplecoding.84412bcc.png"
+  },
+  {
+    "revision": "bd4c370a8b4522518f666b2b6b6b3375",
+    "url": "/mateusz-kapron/static/media/poland.bd4c370a.svg"
+  },
+  {
+    "revision": "efaf0c3d005efeb6afcc054e507bae87",
+    "url": "/mateusz-kapron/static/media/portfolio.efaf0c3d.png"
+  },
+  {
+    "revision": "5e482b88288a5c81c63366b26982ac62",
+    "url": "/mateusz-kapron/static/media/portfolio2.5e482b88.PNG"
+  },
+  {
+    "revision": "ca104dd7e8610e70a8b95ac518bcc284",
+    "url": "/mateusz-kapron/static/media/react-articles.ca104dd7.PNG"
+  },
+  {
+    "revision": "3090aedfb4ae98961ddf18de5f678783",
+    "url": "/mateusz-kapron/static/media/react-corona.3090aedf.PNG"
+  },
+  {
+    "revision": "cc006c6acbe00c68c40de516bc9ea7d4",
+    "url": "/mateusz-kapron/static/media/react-hotel.cc006c6a.PNG"
+  },
+  {
+    "revision": "eab76719fd611cab5eecb1eb1427675a",
+    "url": "/mateusz-kapron/static/media/react.eab76719.svg"
+  },
+  {
+    "revision": "74078620e0a64b0a425a86a42ac8155c",
+    "url": "/mateusz-kapron/static/media/resume.74078620.svg"
+  },
+  {
+    "revision": "6f2c7f84f9e4a15a21aae994180b278d",
+    "url": "/mateusz-kapron/static/media/rwd.6f2c7f84.png"
+  },
+  {
+    "revision": "3023657df2a80a0f8957a339d14ef515",
+    "url": "/mateusz-kapron/static/media/sass.3023657d.svg"
+  },
+  {
+    "revision": "1482617788431c8f50eedd5bdd587351",
+    "url": "/mateusz-kapron/static/media/snake2.14826177.PNG"
+  },
+  {
+    "revision": "1ad712528db03adcd4a989082d1c8259",
+    "url": "/mateusz-kapron/static/media/tenis.1ad71252.PNG"
+  },
+  {
+    "revision": "c235307c3d7304dead387ccf338ffaf4",
+    "url": "/mateusz-kapron/static/media/to-do-app.c235307c.PNG"
+  },
+  {
+    "revision": "a27c7c8e1a4265c90d46e17524141a81",
+    "url": "/mateusz-kapron/static/media/twitters.a27c7c8e.PNG"
+  },
+  {
+    "revision": "79580eb1bff0f0183b343ab953b89c58",
+    "url": "/mateusz-kapron/static/media/typescript.79580eb1.svg"
+  },
+  {
+    "revision": "9b05046ecb00bfb2dacea6f8dd99edbe",
+    "url": "/mateusz-kapron/static/media/weather-aplication-react.9b05046e.PNG"
+  },
+  {
+    "revision": "71125c5a20ef345b73a013606d0ee425",
+    "url": "/mateusz-kapron/static/media/weather-aplication.71125c5a.PNG"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mateusz-kapron/precache-manifest.8c46c45d8dbf720ea0cf7b6412724352.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mateusz-kapron/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
