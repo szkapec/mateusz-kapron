75a34fe25601d8ff91af2409a8d72e263fe93185
szkapec <szkapec999@o2.pl>
2020-12-25 12:19:59 +0100

    Updates

---
+++ precache-manifest.3459478b2f98eaf5456f3fc959ba0f9a.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8f1fa0935ef618adec758d7627c1a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d06cff863e0b971b128b",
+    "url": "/static/css/main.02b31a70.chunk.css"
+  },
+  {
+    "revision": "3bb65749e1b46c2fcfbb",
+    "url": "/static/js/2.ac3e1e8c.chunk.js"
+  },
+  {
+    "revision": "28d36cae8db7790f6fc02920470bed26",
+    "url": "/static/js/2.ac3e1e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d06cff863e0b971b128b",
+    "url": "/static/js/main.bab80482.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "05a474f907105bba3569ebfaed542e0d",
+    "url": "/static/media/Git_icon.05a474f9.svg"
+  },
+  {
+    "revision": "a434edfc66371dc7b76bad8eb9942fa8",
+    "url": "/static/media/Mateusz Kaproń-CV.a434edfc.pdf"
+  },
+  {
+    "revision": "533f381c361f5a8c9ed414a41eb763df",
+    "url": "/static/media/React-node-express2.533f381c.PNG"
+  },
+  {
+    "revision": "6f65452a0230f48bd606026c98adaa54",
+    "url": "/static/media/Roboto.6f65452a.png"
+  },
+  {
+    "revision": "bd1876685d668756086ca7c2110558ec",
+    "url": "/static/media/ajax-icon-7.bd187668.jpg"
+  },
+  {
+    "revision": "6320efd33725ab695dfec6526a2b8ed9",
+    "url": "/static/media/animate.6320efd3.svg"
+  },
+  {
+    "revision": "bea8fe22333a530fd89cc2b638ede39a",
+    "url": "/static/media/appposts.bea8fe22.png"
+  },
+  {
+    "revision": "e9ee74fb80d6987433ce245a5023b4e2",
+    "url": "/static/media/astronomy.e9ee74fb.jpg"
+  },
+  {
+    "revision": "eef73b0030d8cbf608ca8c0f446bbbbf",
+    "url": "/static/media/baflix.eef73b00.png"
+  },
+  {
+    "revision": "d40d337eccb8d7f6650b6b9b35f00191",
+    "url": "/static/media/bootstrap-4.d40d337e.svg"
+  },
+  {
+    "revision": "15e4d4516de38b418fef8fef91bdd321",
+    "url": "/static/media/bowling.15e4d451.PNG"
+  },
+  {
+    "revision": "a26a87ef1f4bfd73c01a431f2a4a8e44",
+    "url": "/static/media/course.a26a87ef.PNG"
+  },
+  {
+    "revision": "2c00067ad9490e2fe47456e03c2e178b",
+    "url": "/static/media/cpp.2c00067a.svg"
+  },
+  {
+    "revision": "cc56234f0da6ca1734348703d458f0d4",
+    "url": "/static/media/css-3.cc56234f.svg"
+  },
+  {
+    "revision": "d8dbb56e8909ef19a59ee7acfec895c8",
+    "url": "/static/media/ecommers2.d8dbb56e.PNG"
+  },
+  {
+    "revision": "5912860130400d430266debbc3b89b07",
+    "url": "/static/media/encrypt-decrypt.59128601.PNG"
+  },
+  {
+    "revision": "cc7bbbbef159fa38e3f823136a37d2d4",
+    "url": "/static/media/english.cc7bbbbe.svg"
+  },
+  {
+    "revision": "d5f66342991130a4c118dffb1f962717",
+    "url": "/static/media/express.d5f66342.png"
+  },
+  {
+    "revision": "ebfe98c9d94439ade2ed31af0e5d6a3b",
+    "url": "/static/media/firebase.ebfe98c9.png"
+  },
+  {
+    "revision": "167ee43207c4d92e47ca22215767a0bb",
+    "url": "/static/media/fullstack2.167ee432.png"
+  },
+  {
+    "revision": "2cc5a3a59af6e9a21c1989fcba16fa6d",
+    "url": "/static/media/generate.2cc5a3a5.PNG"
+  },
+  {
+    "revision": "6e11909cac39a207463d351991c60dbe",
+    "url": "/static/media/github.6e11909c.svg"
+  },
+  {
+    "revision": "30a21c833d9841e5a042fe078e16b03b",
+    "url": "/static/media/html-5.30a21c83.svg"
+  },
+  {
+    "revision": "9c7332a3527805dc84254abd583f8868",
+    "url": "/static/media/java.9c7332a3.svg"
+  },
+  {
+    "revision": "f44b85b706bc652b19ad3dc391140e68",
+    "url": "/static/media/javascript-4.f44b85b7.svg"
+  },
+  {
+    "revision": "5065f87a820b78826dcd79e1fcfb6776",
+    "url": "/static/media/memegenerator.5065f87a.PNG"
+  },
+  {
+    "revision": "f58f6e5865f7ab649dd2ab66c500b48e",
+    "url": "/static/media/mountains-desktop.f58f6e58.jpg"
+  },
+  {
+    "revision": "318a5920f562fd55f928661d3bc53412",
+    "url": "/static/media/mountains-mobile.318a5920.jpg"
+  },
+  {
+    "revision": "692b22662252b65e9644e0352a4240d0",
+    "url": "/static/media/movies.692b2266.png"
+  },
+  {
+    "revision": "23c47370eac545f6ee56afa1293a412e",
+    "url": "/static/media/mysql-5.23c47370.svg"
+  },
+  {
+    "revision": "776738141c96ae63ba34dcf7e3973fbc",
+    "url": "/static/media/nodejs-icon.77673814.svg"
+  },
+  {
+    "revision": "5d59a9f5b86e4720e3f03ae35c893602",
+    "url": "/static/media/people.5d59a9f5.svg"
+  },
+  {
+    "revision": "c9ec2ceee7e8e9f373b1cc652c4078c7",
+    "url": "/static/media/poeplecoding.c9ec2cee.png"
+  },
+  {
+    "revision": "43330bee64d6808f17edb534d7e223cd",
+    "url": "/static/media/poland.43330bee.svg"
+  },
+  {
+    "revision": "5fea76ed96c68fc8fc5ace24113437db",
+    "url": "/static/media/portfolio.5fea76ed.png"
+  },
+  {
+    "revision": "59022ce0d17829e898820f44d81b02f8",
+    "url": "/static/media/portfolio2.59022ce0.PNG"
+  },
+  {
+    "revision": "92bc5556830107d6f99737b9cfe917ed",
+    "url": "/static/media/react-articles.92bc5556.PNG"
+  },
+  {
+    "revision": "2800ade2f382f4d5900623c3f0f9a031",
+    "url": "/static/media/react-corona.2800ade2.PNG"
+  },
+  {
+    "revision": "88917f4e23f93204c7f55ee1a6d9ab4f",
+    "url": "/static/media/react-hotel.88917f4e.PNG"
+  },
+  {
+    "revision": "4d0df56de3689b36f23c19e7cbcb709e",
+    "url": "/static/media/react.4d0df56d.svg"
+  },
+  {
+    "revision": "6640df145fb72c45f192d067ea110458",
+    "url": "/static/media/resume.6640df14.svg"
+  },
+  {
+    "revision": "a51073c01b1b98dafc81d9c011a1a2b1",
+    "url": "/static/media/sass.a51073c0.svg"
+  },
+  {
+    "revision": "6a88eb0fcf4e2efdef4fcf3d2f173243",
+    "url": "/static/media/snake2.6a88eb0f.PNG"
+  },
+  {
+    "revision": "ac0b48c425c80367d73201a90a8fe061",
+    "url": "/static/media/tenis.ac0b48c4.PNG"
+  },
+  {
+    "revision": "48d145d15e686a07301073d717e89bf6",
+    "url": "/static/media/to-do-app.48d145d1.PNG"
+  },
+  {
+    "revision": "576631ec4df886c97854c77b7856bc68",
+    "url": "/static/media/twitters.576631ec.PNG"
+  },
+  {
+    "revision": "e7ef5555cb9dc676ebeb3972cd8db3cf",
+    "url": "/static/media/typescript.e7ef5555.svg"
+  },
+  {
+    "revision": "87d390244bc370e5ac9b1f537857a846",
+    "url": "/static/media/weather-aplication-react.87d39024.PNG"
+  },
+  {
+    "revision": "8341aac2ad323759c2f9472b5f8a3e33",
+    "url": "/static/media/weather-aplication.8341aac2.PNG"
+  }
+]);
